e813f9ae5ddc3a245098cd5934487555beb43f3a
blinikar <blinov.egor2013@mail.ru>
2022-07-05 19:21:55 +0300

    ESlint + Husky installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {SampleComponent} from "./components/SampleComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <SampleComponent text={"This is sample component"} />
-        <h1>Hello world</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SampleComponent text={"This is sample component"} />
+            <h1>Hello world</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {
     BrowserRouter,
     Route,
@@ -10,17 +10,17 @@ import {
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path={"/"} element={<App />} />
-              <Route path={"*"} element={<h1>404</h1>} />
-          </Routes>
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<App />} />
+                <Route path={"*"} element={<h1>404</h1>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
